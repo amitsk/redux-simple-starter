fc392e18ff91aec3c4b356b25be0d40755a0f2a1
amit <amitsk1974@gmail.com>
2018-05-23 23:07:39 -0700

    Updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const API_KEY = '';
+import YTSearch from "youtube-api-search";
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
+
+const API_KEY = 'AIzaSyBOWNtpGja4jJHMQNitMEcPjmjK6qU3424';
+
 
 //Create  a new components
-const App = () => {
-  return <div>Hi! </div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: "surfboards"}, videos => {
+      this.setState({videos});
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+  
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
 
 //Take this component and put in DOM
 
+ 
